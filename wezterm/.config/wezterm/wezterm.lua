ebda6269dd5d11d4d0c5da53a1f0301eb838aae7
Snaylaker <mehdihimmid73@gmail.com>
2024-10-15 21:16:29 +0200

    feat(wezterm) :  removed window title

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,6 +5,10 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 config.hide_tab_bar_if_only_one_tab = true
 
+wezterm.on('format-window-title', function()
+  return ''
+end)
+
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
