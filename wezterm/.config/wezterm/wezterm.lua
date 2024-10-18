b5a49b0e4cd2c27ea93c133530e363eca9f14ddf
Snaylaker <mehdihimmid73@gmail.com>
2024-10-18 21:51:55 +0200

    feat(wezterm) : remove titlebar

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,9 +5,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 config.hide_tab_bar_if_only_one_tab = true
 
-wezterm.on('format-window-title', function()
-  return ''
-end)
+config.window_decorations = 'RESIZE'
 
 -- For example, changing the color scheme:
 config.color_scheme = 'tokyonight'
