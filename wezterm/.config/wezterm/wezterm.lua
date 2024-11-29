b708c0a153366f5871dc9e3b2792852d43331643
Snaylaker <me@private.ma>
2024-11-29 20:33:46 +0100

    theme(pine-rose): switch

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,15 +8,17 @@ config.use_dead_keys = true
 config.use_ime = false
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = 'RESIZE'
-config.color_scheme = 'tokyonight'
+config.color_scheme = 'rose-pine'
 config.font_size = 20
+config.check_for_updates = true
+config.check_for_updates_interval_seconds = 86400
+
 config.window_padding = {
   left = 0,
   right = 0,
   top = 0,
   bottom = 0,
 }
-
 -- Maximize the window on startup
 wezterm.on('gui-startup', function(cmd)
   local pane, tab, window = mux.spawn_window(cmd or {})
